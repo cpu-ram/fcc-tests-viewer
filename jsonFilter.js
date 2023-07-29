2b9676421f88654aff30a349c10ef6bd98188f84
Pete <petr.stepanenko@gmail.com>
2023-07-29 19:17:59 -0400

    refactor the application

--- jsonFilter.js
+++ jsonFilter.js
@@ -1,39 +1,59 @@
 import fs from 'fs';
 import path from 'path';
 
-const studentTest = fs.readFileSync(path.resolve('src/unfiltered-tests-of-student.json'), 'utf-8');
-const exampleTest = fs.readFileSync(path.resolve('src/unfiltered-tests-example.json'), 'utf-8');
-const destination = 'results';
-
-const initialJson = JSON.parse(studentTest);
-
-const filteredRequests = Object
-  .entries(initialJson)[0][1]
-  .entries
-  .filter(
-    (x) => {
-      if (
-        x.request.method === 'POST'
-        && x.request.url.includes('exercises')
-      ) return true;
-      return false;
-    },
-  )
-  .map((x) => (
+// fs.readFileSync(path.resolve(`${srcDirectory}/${fileName}`), 'utf-8')
+const resultsDirectory = 'results';
+const srcDirectory = 'src';
+const srcFileDict = fs
+  .readdirSync(srcDirectory)
+  .map((fileName) => (
     {
-      request: {
-        method: x.request.method,
-        url: x.request.url,
-        body: x.request.postData.text,
-      },
-      response: {
-        status: x.response.status,
-        content: x.response.content.text,
-      },
+      fileName,
+      fileData: fs.readFileSync(path.resolve(`${srcDirectory}/${fileName}`), 'utf-8'),
     }
   ));
-await fs.writeFile(
-  `${destination}/filtered-json.json`,
-  JSON.stringify(filteredRequests),
-  (err) => { if (err !== null) console.log(err); },
-);
+
+const filterFccTestData = async (data) => {
+  const parsedObject = JSON.parse(data);
+
+  const filteredData = Object
+    .entries(parsedObject)[0][1]
+    .entries
+    .filter(
+      (x) => {
+        if (
+          x.request.method === 'POST'
+          && x.request.url.includes('exercises')
+        ) return true;
+        return false;
+      },
+    )
+    .map((x) => (
+      {
+        request: {
+          method: x.request.method,
+          url: x.request.url,
+          body: x.request.postData.text,
+        },
+        response: {
+          status: x.response.status,
+          content: x.response.content.text,
+        },
+      }
+    ));
+  return filteredData;
+};
+const recordFile = async (data, fileName) => {
+  await fs.writeFile(
+    `${resultsDirectory}/filtered-${fileName}`,
+    JSON.stringify(data),
+    (err) => { if (err !== null) console.log(err); },
+  );
+};
+
+srcFileDict.forEach(({ fileName, fileData }) => {
+  filterFccTestData(fileData)
+    .then((result) => {
+      recordFile(result, fileName);
+    });
+});
